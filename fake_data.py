a2df117894ffdc1e6d97120108cef2fbd3c28608
jaimebw <jaimebwv@gmail.com>
2023-04-22 18:58:33 -0400

    Final historical page version

--- fake_data.py
+++ fake_data.py
@@ -11,8 +11,8 @@ import os
 
 Faker.seed(42)
 NUMBER_OF_USERS = 5
-N_PRODUCTS = 5
-N_DAYS = 5
+N_PRODUCTS = 10
+N_DAYS = 20
 
 
 def create_db():
@@ -36,6 +36,14 @@ def generate_asin():
     
     # Return the ASIN
     return asin
+
+def sum_bin_id(binary_id, n):
+    # Convert binary ID to integer and add n
+    int_id = int.from_bytes(binary_id, byteorder='big') + n
+    # Convert integer ID back to binary
+    updated_binary_id = int_id.to_bytes(16, byteorder='big')
+    # Return the sum of the integer ID and n and the updated binary ID
+    return  updated_binary_id
 
 
 def random_id():
@@ -83,103 +91,108 @@ for i in range(NUMBER_OF_USERS):
     cur.execute("INSERT INTO Manufacturer(ManuID, name) VALUES (?,?) ",
             manufacturers)
     # Sold_Product_Manufactured
-    pid = random_id()
-    url = fake.url()
-    product_name = " ".join(fake.words(nb=2, unique=True))
-    sold_product_manufactured = [
-        pid,
-        url,
-        product_name,
-        depid,
-        manuid
-            ]
-    cur.execute("INSERT INTO Sold_Product_Manufactured(PID,URL,name,DepID,ManuID) \
-            VALUES (?,?,?,?,?)",sold_product_manufactured)
 
-    # Tracks
-    cur.execute("INSERT INTO Tracks(sid,PID) \
-            VALUES (?,?)",[sid,pid])
-
-    #Productt variants sold_product_manufactured
-    asin = generate_asin()
-    print(asin)
-    variation = random.choice(["first_var","second_var","none"])
-    ttype = "none"
-    product_variant_sold = [
-            asin,
-            variation,
-            ttype,
-            pid
-            ]
-    cur.execute("INSERT INTO product_variant_sold(ASIN,variation,type,PID) \
-            VALUES (?,?,?,?)",product_variant_sold)
-    # Deal Alert on
-    # Change conditions to be random
-    conditions = random.choice(["Good","Bad","Excellent","Fair"])
-    last_notification = fake.date()
-    deal_alerts_on = [conditions,asin,last_notification]
-    cur.execute("INSERT INTO Deal_Alert_on(conditions,ASIN,last_notification)\
-            VALUES (?,?,?)",deal_alerts_on)
-    # Subscribes_To
-    subscribes_to = [
-            conditions,
-            asin,
-            sid
-            ]
-    cur.execute("INSERT INTO Subscribes_To(conditions,ASIN,sid) \
-            VALUES (?,?,?)", subscribes_to)
-    # Area withing
-    cur.execute("INSERT INTO Area_within(sub_DepID,Category_DepID) \
-            VALUES (?,?)", [depid,depid])
-    # Contains reviews
-    rating = random.randint(1, 5) 
-    review_date  = fake.date_between(start_date='-3m', end_date='today')
-    contains_reviews = [asin,pid,rating,review_date]
-    cur.execute("INSERT INTO Contains_Reviews(ASIN,PID,rating,reviewdate)\
-            VALUES (?,?,?,?)",contains_reviews)
-    
-    # Ranked best seller rank
-    rank = str(random.randint(1,5)) # This might be wrong in the sql schema
-    category = random.choice(["House","Videogames","Kitchen"])
-    ranked_best = [rank,asin,category]
-    cur.execute("INSERT INTO Ranked_Best_Seller_Rank(rank,ASIN,category) \
-            VALUES (?,?,?)", ranked_best)
-    # Company
-    comid = random_id()
-    comp_name = fake.company()
-    company = [comid,comp_name]
-    cur.execute("INSERT INTO Company(ComID,name) \
-                VALUES (?,?)", company)
-
-    conn.commit()
-    # For_Product_Data_Refres
-    datetime0 =  fake.date_between(start_date='-1m', end_date='today')
-    for day in range(N_DAYS):
-        if day == 0:
-           datetime = datetime0 
-        else:
-            datetime = datetime0 + timedelta(days = day) 
-        #print(f"Date:{datetime}, asin:{asin}")
-
-
-        cur.execute("INSERT INTO For_Product_Data_Refresh(datetime,ASIN) \
-                VALUES (?,?)", [datetime,asin])
-        # Has_Listing_collected
-        listing_id = random_id()
-        price = round(random.uniform(2,50))
-        has_listing_collected = [
-                listing_id,
-                asin,
-                conditions,
-                price,
-                datetime,
-                comid,
-                comid
+    for prod in range(N_PRODUCTS):
+        # Generate more products per user
+        pid = random_id()
+        url = fake.url()
+        product_name = " ".join(fake.words(nb=2, unique=True))
+        sold_product_manufactured = [
+            pid,
+            url,
+            product_name,
+            depid,
+            manuid
                 ]
+        cur.execute("INSERT INTO Sold_Product_Manufactured(PID,URL,name,DepID,ManuID) \
+                VALUES (?,?,?,?,?)",sold_product_manufactured)
 
-        cur.execute("INSERT INTO Has_Listing_collected(ListingID,ASIN,condition,\
-                Price,datetime,shipped_comID,sold_ComID) VALUES (?,?,?,?,?,?,?)",has_listing_collected)
+        # Tracks
+        cur.execute("INSERT INTO Tracks(sid,PID) \
+                VALUES (?,?)",[sid,pid])
+        depid = sum_bin_id(depid,prod)
+
+
+        #Productt variants sold_product_manufactured
+        asin = generate_asin()
+        print(asin)
+        variation = random.choice(["first_var","second_var","none"])
+        ttype = "none"
+        product_variant_sold = [
+                asin,
+                variation,
+                ttype,
+                pid
+                ]
+        cur.execute("INSERT INTO product_variant_sold(ASIN,variation,type,PID) \
+                VALUES (?,?,?,?)",product_variant_sold)
+        # Deal Alert on
+        # Change conditions to be random
+        conditions = random.choice(["Good","Bad","Excellent","Fair"])
+        last_notification = fake.date()
+        deal_alerts_on = [conditions,asin,last_notification]
+        cur.execute("INSERT INTO Deal_Alert_on(conditions,ASIN,last_notification)\
+                VALUES (?,?,?)",deal_alerts_on)
+        # Subscribes_To
+        subscribes_to = [
+                conditions,
+                asin,
+                sid
+                ]
+        cur.execute("INSERT INTO Subscribes_To(conditions,ASIN,sid) \
+                VALUES (?,?,?)", subscribes_to)
+        # Area withing
+        cur.execute("INSERT INTO Area_within(sub_DepID,Category_DepID) \
+                VALUES (?,?)", [depid,depid])
+        # Contains reviews
+        rating = random.randint(1, 5) 
+        review_date  = fake.date_between(start_date='-3m', end_date='today')
+        contains_reviews = [asin,pid,rating,review_date]
+        cur.execute("INSERT INTO Contains_Reviews(ASIN,PID,rating,reviewdate)\
+                VALUES (?,?,?,?)",contains_reviews)
+        
+        # Ranked best seller rank
+        rank = str(random.randint(1,5)) # This might be wrong in the sql schema
+        category = random.choice(["House","Videogames","Kitchen"])
+        ranked_best = [rank,asin,category]
+        cur.execute("INSERT INTO Ranked_Best_Seller_Rank(rank,ASIN,category) \
+                VALUES (?,?,?)", ranked_best)
+        # Company
+        comid = random_id()
+        comp_name = fake.company()
+        company = [comid,comp_name]
+        cur.execute("INSERT INTO Company(ComID,name) \
+                    VALUES (?,?)", company)
+
         conn.commit()
+        # For_Product_Data_Refres
+        datetime0 =  fake.date_between(start_date='-60d', end_date='today')
+        for day in range(N_DAYS):
+            if day == 0:
+               datetime = datetime0 
+            else:
+                datetime = datetime0 + timedelta(days = day) 
+            #print(f"Date:{datetime}, asin:{asin}")
+
+
+            cur.execute("INSERT INTO For_Product_Data_Refresh(datetime,ASIN) \
+                    VALUES (?,?)", [datetime,asin])
+            # Has_Listing_collected
+            listing_id = random_id()
+            price = round(random.uniform(2,50))
+            has_listing_collected = [
+                    listing_id,
+                    asin,
+                    conditions,
+                    price,
+                    datetime,
+                    comid,
+                    comid
+                    ]
+
+            cur.execute("INSERT INTO Has_Listing_collected(ListingID,ASIN,condition,\
+                    Price,datetime,shipped_comID,sold_ComID) VALUES (?,?,?,?,?,?,?)",has_listing_collected)
+            conn.commit()
 
 
 
